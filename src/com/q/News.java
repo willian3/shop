984e1ca89b0dd8b1cd68a64345afec4087767068
willian3 <1158560457@qq.com>
2017-10-15 23:08:47 +0800

    ceshi

---
+++ src/com/q/News.java
@@ -0,0 +1,13 @@
+package com.q;
+
+public class News {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("ceshi");
+	}
+
+}
